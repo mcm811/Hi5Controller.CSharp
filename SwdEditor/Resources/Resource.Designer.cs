3968109d942bcdaa58125083124e79d3002820ee
Changmin Moon <changmin811@gmail.com>
2015-09-02 05:04:47 +0900

    swd 파일 읽기(분석) 추가. swd 파싱 성능 개선

--- SwdEditor/Resources/Resource.Designer.cs
+++ SwdEditor/Resources/Resource.Designer.cs
@@ -31,6 +31,39 @@ namespace SwdEditor
 		public partial class Attribute
 		{
 			
+			// aapt resource value: 0x7f010000
+			public const int cardBackgroundColor = 2130771968;
+			
+			// aapt resource value: 0x7f010001
+			public const int cardCornerRadius = 2130771969;
+			
+			// aapt resource value: 0x7f010002
+			public const int cardElevation = 2130771970;
+			
+			// aapt resource value: 0x7f010003
+			public const int cardMaxElevation = 2130771971;
+			
+			// aapt resource value: 0x7f010005
+			public const int cardPreventCornerOverlap = 2130771973;
+			
+			// aapt resource value: 0x7f010004
+			public const int cardUseCompatPadding = 2130771972;
+			
+			// aapt resource value: 0x7f010006
+			public const int contentPadding = 2130771974;
+			
+			// aapt resource value: 0x7f01000a
+			public const int contentPaddingBottom = 2130771978;
+			
+			// aapt resource value: 0x7f010007
+			public const int contentPaddingLeft = 2130771975;
+			
+			// aapt resource value: 0x7f010008
+			public const int contentPaddingRight = 2130771976;
+			
+			// aapt resource value: 0x7f010009
+			public const int contentPaddingTop = 2130771977;
+			
 			static Attribute()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
@@ -41,11 +74,73 @@ namespace SwdEditor
 			}
 		}
 		
+		public partial class Color
+		{
+			
+			// aapt resource value: 0x7f040000
+			public const int cardview_dark_background = 2130968576;
+			
+			// aapt resource value: 0x7f040001
+			public const int cardview_light_background = 2130968577;
+			
+			// aapt resource value: 0x7f040002
+			public const int cardview_shadow_end_color = 2130968578;
+			
+			// aapt resource value: 0x7f040003
+			public const int cardview_shadow_start_color = 2130968579;
+			
+			// aapt resource value: 0x7f040004
+			public const int my_blue = 2130968580;
+			
+			// aapt resource value: 0x7f040007
+			public const int my_gray = 2130968583;
+			
+			// aapt resource value: 0x7f040005
+			public const int my_green = 2130968581;
+			
+			// aapt resource value: 0x7f040006
+			public const int my_purple = 2130968582;
+			
+			static Color()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Color()
+			{
+			}
+		}
+		
+		public partial class Dimension
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int cardview_compat_inset_shadow = 2131034112;
+			
+			// aapt resource value: 0x7f050001
+			public const int cardview_default_elevation = 2131034113;
+			
+			// aapt resource value: 0x7f050002
+			public const int cardview_default_radius = 2131034114;
+			
+			static Dimension()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Dimension()
+			{
+			}
+		}
+		
 		public partial class Drawable
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			public const int box = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int Icon = 2130837505;
 			
 			static Drawable()
 			{
@@ -60,95 +155,116 @@ namespace SwdEditor
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f05001d
-			public const int analogClock1 = 2131034141;
+			// aapt resource value: 0x7f080018
+			public const int beginSeekBar = 2131230744;
 			
-			// aapt resource value: 0x7f050003
-			public const int editText1 = 2131034115;
+			// aapt resource value: 0x7f080021
+			public const int cardText = 2131230753;
 			
-			// aapt resource value: 0x7f050006
-			public const int editText2 = 2131034118;
+			// aapt resource value: 0x7f08001f
+			public const int cardView = 2131230751;
 			
-			// aapt resource value: 0x7f050009
-			public const int editText3 = 2131034121;
+			// aapt resource value: 0x7f080004
+			public const int editText1 = 2131230724;
 			
-			// aapt resource value: 0x7f05000c
-			public const int editText4 = 2131034124;
+			// aapt resource value: 0x7f080007
+			public const int editText2 = 2131230727;
 			
-			// aapt resource value: 0x7f05000f
-			public const int editText5 = 2131034127;
+			// aapt resource value: 0x7f08000a
+			public const int editText3 = 2131230730;
 			
-			// aapt resource value: 0x7f050012
-			public const int editText6 = 2131034130;
+			// aapt resource value: 0x7f08000d
+			public const int editText4 = 2131230733;
 			
-			// aapt resource value: 0x7f050015
-			public const int editText7 = 2131034133;
+			// aapt resource value: 0x7f080010
+			public const int editText5 = 2131230736;
 			
-			// aapt resource value: 0x7f050018
-			public const int editText8 = 2131034136;
+			// aapt resource value: 0x7f080013
+			public const int editText6 = 2131230739;
 			
-			// aapt resource value: 0x7f05001b
-			public const int editText9 = 2131034139;
+			// aapt resource value: 0x7f080016
+			public const int editText7 = 2131230742;
 			
-			// aapt resource value: 0x7f05001c
-			public const int linearLayout1 = 2131034140;
+			// aapt resource value: 0x7f080019
+			public const int editText8 = 2131230745;
 			
-			// aapt resource value: 0x7f050017
-			public const int seekBar1 = 2131034135;
+			// aapt resource value: 0x7f08001c
+			public const int editText9 = 2131230748;
 			
-			// aapt resource value: 0x7f05001a
-			public const int seekBar2 = 2131034138;
+			// aapt resource value: 0x7f08001b
+			public const int endSeekBar = 2131230747;
 			
-			// aapt resource value: 0x7f050000
-			public const int tableLayout1 = 2131034112;
+			// aapt resource value: 0x7f080020
+			public const int imageButton1 = 2131230752;
 			
-			// aapt resource value: 0x7f050001
-			public const int tableRow1 = 2131034113;
+			// aapt resource value: 0x7f08001e
+			public const int linearLayout1 = 2131230750;
 			
-			// aapt resource value: 0x7f050004
-			public const int tableRow2 = 2131034116;
+			// aapt resource value: 0x7f080022
+			public const int openButton = 2131230754;
 			
-			// aapt resource value: 0x7f050007
-			public const int tableRow3 = 2131034119;
+			// aapt resource value: 0x7f080000
+			public const int sampleSeekBar = 2131230720;
 			
-			// aapt resource value: 0x7f05000a
-			public const int tableRow4 = 2131034122;
+			// aapt resource value: 0x7f080024
+			public const int scrollTextView = 2131230756;
 			
-			// aapt resource value: 0x7f050013
-			public const int tableRow5 = 2131034131;
+			// aapt resource value: 0x7f080023
+			public const int scrollView1 = 2131230755;
 			
-			// aapt resource value: 0x7f050010
-			public const int tableRow6 = 2131034128;
+			// aapt resource value: 0x7f08001d
+			public const int statusText = 2131230749;
 			
-			// aapt resource value: 0x7f05000d
-			public const int tableRow7 = 2131034125;
+			// aapt resource value: 0x7f080001
+			public const int tableLayout1 = 2131230721;
 			
-			// aapt resource value: 0x7f050016
-			public const int tableRow8 = 2131034134;
+			// aapt resource value: 0x7f080002
+			public const int tableRow1 = 2131230722;
 			
-			// aapt resource value: 0x7f050019
-			public const int tableRow9 = 2131034137;
+			// aapt resource value: 0x7f080005
+			public const int tableRow2 = 2131230725;
 			
-			// aapt resource value: 0x7f050002
-			public const int textView1 = 2131034114;
+			// aapt resource value: 0x7f080008
+			public const int tableRow3 = 2131230728;
 			
-			// aapt resource value: 0x7f050005
-			public const int textView2 = 2131034117;
+			// aapt resource value: 0x7f08000b
+			public const int tableRow4 = 2131230731;
 			
-			// aapt resource value: 0x7f050008
-			public const int textView3 = 2131034120;
+			// aapt resource value: 0x7f080014
+			public const int tableRow5 = 2131230740;
 			
-			// aapt resource value: 0x7f05000b
-			public const int textView4 = 2131034123;
+			// aapt resource value: 0x7f080011
+			public const int tableRow6 = 2131230737;
 			
-			// aapt resource value: 0x7f05000e
-			public const int textView5 = 2131034126;
+			// aapt resource value: 0x7f08000e
+			public const int tableRow7 = 2131230734;
 			
-			// aapt resource value: 0x7f050011
-			public const int textView6 = 2131034129;
+			// aapt resource value: 0x7f080017
+			public const int tableRow8 = 2131230743;
 			
-			// aapt resource value: 0x7f050014
-			public const int textView7 = 2131034132;
+			// aapt resource value: 0x7f08001a
+			public const int tableRow9 = 2131230746;
+			
+			// aapt resource value: 0x7f080003
+			public const int textView1 = 2131230723;
+			
+			// aapt resource value: 0x7f080006
+			public const int textView2 = 2131230726;
+			
+			// aapt resource value: 0x7f080009
+			public const int textView3 = 2131230729;
+			
+			// aapt resource value: 0x7f08000c
+			public const int textView4 = 2131230732;
+			
+			// aapt resource value: 0x7f08000f
+			public const int textView5 = 2131230735;
+			
+			// aapt resource value: 0x7f080012
+			public const int textView6 = 2131230738;
+			
+			// aapt resource value: 0x7f080015
+			public const int textView7 = 2131230741;
 			
 			static Id()
 			{
@@ -164,13 +280,13 @@ namespace SwdEditor
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int EditField = 2130903040;
+			public const int CardView = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int Main = 2130903041;
+			public const int Editor = 2130903041;
 			
 			// aapt resource value: 0x7f030002
-			public const int Swd = 2130903042;
+			public const int Main = 2130903042;
 			
 			static Layout()
 			{
@@ -185,11 +301,11 @@ namespace SwdEditor
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int ApplicationName = 2130968577;
+			// aapt resource value: 0x7f070001
+			public const int ApplicationName = 2131165185;
 			
-			// aapt resource value: 0x7f040000
-			public const int Hello = 2130968576;
+			// aapt resource value: 0x7f070000
+			public const int Hello = 2131165184;
 			
 			static String()
 			{
@@ -197,6 +313,90 @@ namespace SwdEditor
 			}
 			
 			private String()
+			{
+			}
+		}
+		
+		public partial class Style
+		{
+			
+			// aapt resource value: 0x7f060000
+			public const int CardView = 2131099648;
+			
+			// aapt resource value: 0x7f060001
+			public const int CardView_Dark = 2131099649;
+			
+			// aapt resource value: 0x7f060002
+			public const int CardView_Light = 2131099650;
+			
+			// aapt resource value: 0x7f060003
+			public const int MyCustomTheme = 2131099651;
+			
+			static Style()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Style()
+			{
+			}
+		}
+		
+		public partial class Styleable
+		{
+			
+			public static int[] CardView = new int[] {
+					2130771968,
+					2130771969,
+					2130771970,
+					2130771971,
+					2130771972,
+					2130771973,
+					2130771974,
+					2130771975,
+					2130771976,
+					2130771977,
+					2130771978};
+			
+			// aapt resource value: 0
+			public const int CardView_cardBackgroundColor = 0;
+			
+			// aapt resource value: 1
+			public const int CardView_cardCornerRadius = 1;
+			
+			// aapt resource value: 2
+			public const int CardView_cardElevation = 2;
+			
+			// aapt resource value: 3
+			public const int CardView_cardMaxElevation = 3;
+			
+			// aapt resource value: 5
+			public const int CardView_cardPreventCornerOverlap = 5;
+			
+			// aapt resource value: 4
+			public const int CardView_cardUseCompatPadding = 4;
+			
+			// aapt resource value: 6
+			public const int CardView_contentPadding = 6;
+			
+			// aapt resource value: 10
+			public const int CardView_contentPaddingBottom = 10;
+			
+			// aapt resource value: 7
+			public const int CardView_contentPaddingLeft = 7;
+			
+			// aapt resource value: 8
+			public const int CardView_contentPaddingRight = 8;
+			
+			// aapt resource value: 9
+			public const int CardView_contentPaddingTop = 9;
+			
+			static Styleable()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Styleable()
 			{
 			}
 		}
--- SwdEditor/SwdActivity.cs
+++ SwdEditor/SwdActivity.cs
@@ -17,48 +17,45 @@ using System.Threading.Tasks;
 
 namespace SwdEditor
 {
-	[Activity(Label = "용접 조건 데이터 (Weld condition data)", MainLauncher = false, Icon = "@drawable/icon")]
+	[Activity(Label = "용접 조건 데이터 (Weld condition data)", MainLauncher = false, Icon = "@drawable/icon", Theme = "@style/MyCustomTheme")]
 	public class SwdActivity : Activity
 	{
+		static readonly List<WeldConditionData> weldConditonDataList = new List<WeldConditionData>();
+
 		string[] stringTitle = new string[] {
-			"출력 데이터,출력 타입,가압력,이동극 제거율,고정극 제거율,패널 두께,명령 옵셋",
 			//"Output data,Output type,Squeeze force,Move tip clearance,Fixed tip clearence,Pannel thickness,Command offset",
+			//"출력 데이터,출력 타입,가압력,이동극 제거율,고정극 제거율,패널 두께,명령 옵셋",
+			"출력 타입,가압력,이동극 제거율,고정극 제거율,패널 두께,명령 옵셋",
 		};
 
 		private const int columnWidth = 100;
 
 		private bool IsVertical() {
-			return false;
-			//return (WindowManager.DefaultDisplay.Rotation == SurfaceOrientation.Rotation0 || WindowManager.DefaultDisplay.Rotation == SurfaceOrientation.Rotation180);
+			var metrics = Resources.DisplayMetrics;
+			//bool vt = (WindowManager.DefaultDisplay.Rotation == SurfaceOrientation.Rotation0 || WindowManager.DefaultDisplay.Rotation == SurfaceOrientation.Rotation180);
+			//Console.WriteLine("넓이:{0}, 높이:{1}", metrics.WidthPixels, metrics.HeightPixels);
+			return metrics.WidthPixels < metrics.HeightPixels;
 		}
 
-		private TableLayout titleTable;
-
 		private TableLayout CreateTitleTable(bool StretchAllColumns = true, bool ShrinkAllColumns = true) {
-			titleTable = new TableLayout(this);
+			TableLayout titleTable = new TableLayout(this);
 			titleTable.StretchAllColumns = StretchAllColumns;
 			titleTable.ShrinkAllColumns = ShrinkAllColumns;
 
-			//String udata = "Underlined Text";
-			//SpannableString content = new SpannableString(udata);
-			//content.SetSpan(new UnderlineSpan(), 0, udata.Length, 0);
-			//mTextView.setText(content);
-
+			bool isVertical = IsVertical();
 			int n = 0;
 			foreach (string item in stringTitle) {
 				TableRow rowItem = new TableRow(this);
 
 				CheckBox checkBox = new CheckBox(this);
-				checkBox.Text = n == 0 ? "No" : "Index";
+				checkBox.Text = n++ == 0 ? "No" : "Index";
 				checkBox.PaintFlags = PaintFlags.UnderlineText;
-                checkBox.SetTextSize(Android.Util.ComplexUnitType.Pt, 6);
+				checkBox.SetTextSize(Android.Util.ComplexUnitType.Pt, 6);
 				checkBox.SetWidth(columnWidth);
-				//checkBox.SetTextColor(Color.Rgb(255, 255, 255));
-				//checkBox.SetBackgroundColor(Color.Rgb(200, 200, 200));
 
 				rowItem.AddView(checkBox);
 
-				int nValue = 0;
+				int nv = 0;
 				string[] data = item.Trim().Split(new char[] { ',', '-' });
 				foreach (string value in data) {
 					if (value.Length != 0) {
@@ -69,15 +66,11 @@ namespace SwdEditor
 						valueText.SetWidth(columnWidth);
 						valueText.PaintFlags = PaintFlags.UnderlineText;
 						valueText.SetTextSize(Android.Util.ComplexUnitType.Pt, 6);
-						//valueText.SetTextColor(Color.Rgb(255, 255, 255));
-						//valueText.SetBackgroundColor(Color.Rgb(200, 200, 200));
-						if (IsVertical() && nValue < 2) valueText.Visibility = ViewStates.Gone;
+						if (isVertical && nv++ < 1) valueText.Visibility = ViewStates.Gone;
 						rowItem.AddView(valueText);
-						nValue++;
 					}
 				}
 				titleTable.AddView(rowItem);
-				n++;
 			}
 
 			return titleTable;
@@ -90,31 +83,41 @@ namespace SwdEditor
 			mainTable.StretchAllColumns = StretchAllColumns;
 			mainTable.ShrinkAllColumns = ShrinkAllColumns;
 
-			int mainTableIndex = 1;
+			//WeldConditionData wcd = new WeldConditionData();
+
+            bool isVertical = IsVertical();
 			var weldConditionData = Intent.Extras.GetStringArrayList("weld_condition_data") ?? new string[0];
 			foreach (string item in weldConditionData) {
+				string[] its = item.Trim().Split(new char[] { '=' });
+				if (its.Length != 2) {
+					Log.Error("SWD: its - ", its.Length.ToString() + ": [" + its[0] + "] [" + its[1] + "]");
+					continue;
+				}
+
+				string[] data = its[1].Trim().Split(new char[] { ',', '-' });
+				if (data.Length != 7) {
+					Log.Error("SWD: data - ", data.Length.ToString() + ": [" + data[0] + "] [" + data[1] + "]");
+					continue;
+				}
+
 				TableRow rowItem = new TableRow(this);
-
-				CheckBox checkBox = new CheckBox(this);
-				checkBox.Text = mainTableIndex++.ToString();
-				checkBox.SetTextSize(Android.Util.ComplexUnitType.Pt, 6);
-				checkBox.SetWidth(columnWidth);
-
-				rowItem.AddView(checkBox);
-
-				int nValue = 0;
-				string[] data = item.Trim().Split(new char[] { ',', '-' });
+				int nv = 0;
 				foreach (string value in data) {
-					if (value.Length != 0) {
-						TextView valueText = new TextView(this);
-						valueText.SetText(value.Trim(), TextView.BufferType.Editable);
-						valueText.Gravity = GravityFlags.Center;
-						valueText.SetSingleLine(true);
-						valueText.SetWidth(columnWidth);
-						if (IsVertical() && nValue < 2) valueText.Visibility = ViewStates.Gone;
-						rowItem.AddView(valueText);
-						nValue++;
+					if (nv == 0) {
+						CheckBox view = new CheckBox(this);
+						view.Text = value;
+						view.SetWidth(columnWidth);
+						view.SetTextSize(Android.Util.ComplexUnitType.Pt, 6);
+						rowItem.AddView(view);
+					} else {
+						TextView view = new TextView(this);
+						view.Text = value;
+						view.SetWidth(columnWidth);
+						view.Gravity = GravityFlags.Center;
+						if (isVertical && nv < 2) view.Visibility = ViewStates.Gone;
+						rowItem.AddView(view);
 					}
+					nv++;
 				}
 
 				rowItem.Click += (sender, e) => {
@@ -125,7 +128,7 @@ namespace SwdEditor
 				rowItem.LongClick += (sender, e) => {
 					var dialog = new AlertDialog.Builder(this);
 					LayoutInflater layoutInflater = LayoutInflater.From(this);
-					View editFieldView = layoutInflater.Inflate(Resource.Layout.EditField, null);
+					View editFieldView = layoutInflater.Inflate(Resource.Layout.Editor, null);
 					dialog.SetView(editFieldView);
 
 					List<TextView> textView = new List<TextView>();
@@ -149,8 +152,7 @@ namespace SwdEditor
 					editText.Add(editFieldView.FindViewById<EditText>(Resource.Id.editText9));
 
 					for (int i = 1; i < rowItem.ChildCount; i++) {
-						TextView tv = (TextView)rowItem.GetChildAt(i);
-						editText[i - 1].Text = tv.Text;
+						editText[i - 1].Text = ((TextView)rowItem.GetChildAt(i)).Text;
 					}
 
 					for (int i = 0; i < textView.Count; i++) {
@@ -161,49 +163,101 @@ namespace SwdEditor
 						};
 					}
 
-					var _seekBar1 = editFieldView.FindViewById<SeekBar>(Resource.Id.seekBar1);
-					_seekBar1.Max = mainTable.ChildCount - 1;
-					_seekBar1.Progress = 0;
-					editText[7].Text = string.Format("{0}", _seekBar1.Progress + 1);
+					var statusText = editFieldView.FindViewById<TextView>(Resource.Id.statusText);
+					var sampleSeekBar = editFieldView.FindViewById<SeekBar>(Resource.Id.sampleSeekBar);
 
-					var _seekBar2 = editFieldView.FindViewById<SeekBar>(Resource.Id.seekBar2);
-					_seekBar2.Max = mainTable.ChildCount - 1;
-					_seekBar2.Progress = _seekBar2.Max;
-
-					_seekBar1.ProgressChanged += (object sender1, SeekBar.ProgressChangedEventArgs e1) => {
-						if (e1.FromUser) {
-							editText[7].Text = string.Format("{0}", e1.Progress + 1);
-							editText[7].Visibility = _seekBar1.Progress == _seekBar1.Max ? ViewStates.Invisible : ViewStates.Visible;
-							editText[8].Visibility = _seekBar2.Progress == 0 ? ViewStates.Invisible : ViewStates.Visible;
-							editText[7].Visibility = _seekBar1.Progress > _seekBar2.Progress ? ViewStates.Invisible : ViewStates.Visible;
-							editText[8].Visibility = _seekBar2.Progress < _seekBar1.Progress ? ViewStates.Invisible : ViewStates.Visible;
+					sampleSeekBar.Max = mainTable.ChildCount - 1;
+					sampleSeekBar.Progress = Convert.ToInt32(((CheckBox)rowItem.GetChildAt(0)).Text) - 1;
+					statusText.Text = sampleSeekBar.Progress.ToString();
+					sampleSeekBar.ProgressChanged += (object sender1, SeekBar.ProgressChangedEventArgs e1) => {
+						statusText.Text = (e1.Progress + 1).ToString();
+						TableRow tri = (TableRow)mainTable.GetChildAt(sampleSeekBar.Progress);
+						for (int i = 1; i < tri.ChildCount; i++) {
+							editText[i - 1].Text = ((TextView)tri.GetChildAt(i)).Text;
+							editText[i - 1].Visibility = ViewStates.Visible;
 						}
 					};
 
-					editText[8].Text = string.Format("{0}", _seekBar2.Progress + 1);
-					_seekBar2.ProgressChanged += (object sender2, SeekBar.ProgressChangedEventArgs e2) => {
+					var beginSeekBar = editFieldView.FindViewById<SeekBar>(Resource.Id.beginSeekBar);
+					var endSeekBar = editFieldView.FindViewById<SeekBar>(Resource.Id.endSeekBar);
+
+					beginSeekBar.Max = mainTable.ChildCount;
+					beginSeekBar.Progress = beginSeekBar.Max / 2;
+
+					endSeekBar.Max = mainTable.ChildCount;
+					endSeekBar.Progress = endSeekBar.Max / 2;
+
+					editText[7].Text = string.Format("{0}", beginSeekBar.Progress + 1);
+					editText[8].Text = string.Format("{0}", endSeekBar.Progress + 1);
+
+					editText[7].TextChanged += (object sender1, TextChangedEventArgs e1) => {
+						//statusText.Text = e1.Text.ToString();
+						int n = Convert.ToInt32(e1.Text.ToString());
+						if (n > beginSeekBar.Max) {
+							n = beginSeekBar.Max;
+							editText[7].Text = n.ToString();
+						}
+						beginSeekBar.Progress = n;
+					};
+					editText[8].TextChanged += (object sender1, TextChangedEventArgs e1) => {
+						//statusText.Text = e1.Text.ToString();
+						int n = Convert.ToInt32(e1.Text.ToString());
+						if (n > endSeekBar.Max) {
+							n = endSeekBar.Max;
+							editText[8].Text = n.ToString();
+						}
+						endSeekBar.Progress = endSeekBar.Max - n - 1;
+					};
+
+					beginSeekBar.ProgressChanged += (object sender1, SeekBar.ProgressChangedEventArgs e1) => {
+						if (e1.FromUser) {
+							int sb1Progress = beginSeekBar.Progress;
+							int sb2Progress = endSeekBar.Max - endSeekBar.Progress;
+							editText[7].Text = string.Format("{0}", sb1Progress + 1);
+							if (sb1Progress > sb2Progress) {
+								endSeekBar.Progress = endSeekBar.Max - beginSeekBar.Progress;
+								editText[8].Text = string.Format("{0}", sb1Progress + 1);
+							}
+							if (sb1Progress == 1 && sb2Progress == 1 || sb1Progress == beginSeekBar.Max && sb2Progress == endSeekBar.Max) {
+								editText[7].Visibility = ViewStates.Invisible;
+								editText[8].Visibility = ViewStates.Invisible;
+							} else {
+								editText[7].Visibility = ViewStates.Visible;
+								editText[8].Visibility = ViewStates.Visible;
+							}
+						}
+					};
+
+					endSeekBar.ProgressChanged += (object sender2, SeekBar.ProgressChangedEventArgs e2) => {
 						if (e2.FromUser) {
-							editText[8].Text = string.Format("{0}", e2.Progress + 1);
-							editText[7].Visibility = _seekBar1.Progress == _seekBar1.Max ? ViewStates.Invisible : ViewStates.Visible;
-							editText[8].Visibility = _seekBar2.Progress == 0 ? ViewStates.Invisible : ViewStates.Visible;
-							editText[7].Visibility = _seekBar1.Progress > _seekBar2.Progress ? ViewStates.Invisible : ViewStates.Visible;
-							editText[8].Visibility = _seekBar2.Progress < _seekBar1.Progress ? ViewStates.Invisible : ViewStates.Visible;
+							int sb1Progress = beginSeekBar.Progress;
+							int sb2Progress = endSeekBar.Max - endSeekBar.Progress;
+							editText[8].Text = string.Format("{0}", sb2Progress + 1);
+							if (sb1Progress > sb2Progress) {
+								beginSeekBar.Progress = sb2Progress;
+								editText[7].Text = string.Format("{0}", sb2Progress + 1);
+							}
+							if (sb1Progress <= 1 && sb2Progress <= 1 || sb1Progress >= beginSeekBar.Max && sb2Progress >= endSeekBar.Max) {
+								editText[7].Visibility = ViewStates.Invisible;
+								editText[8].Visibility = ViewStates.Invisible;
+							} else {
+								editText[7].Visibility = ViewStates.Visible;
+								editText[8].Visibility = ViewStates.Visible;
+							}
 						}
 					};
 
 					int checkCount = 0;
 					// 체크가 된 항목 갯수
 					for (int i = 0; i < mainTable.ChildCount; i++) {
-						TableRow ri = (TableRow)mainTable.GetChildAt(i);
-						CheckBox cb = (CheckBox)ri.GetChildAt(0);
+						CheckBox cb = (CheckBox)((TableRow)mainTable.GetChildAt(i)).GetChildAt(0);
 						if (cb.Checked) checkCount++;
 					}
 
 					dialog.SetNegativeButton("취소", delegate {
 						if (checkCount > 0) {
 							for (int i = 0; i < mainTable.ChildCount; i++) {
-								TableRow ri = (TableRow)mainTable.GetChildAt(i);
-								CheckBox cb = (CheckBox)ri.GetChildAt(0);
+								CheckBox cb = (CheckBox)((TableRow)mainTable.GetChildAt(i)).GetChildAt(0);
 								cb.Checked = false;
 							}
 						}
@@ -256,6 +310,13 @@ namespace SwdEditor
 								}
 							}
 						}
+
+						/*
+						// 다시 메인 액티비티 호출
+						var intent = new Intent(this, typeof(MainActivity));
+						//intent.PutStringArrayListExtra("weld_condition_data", weldConditionData);
+						StartActivity(intent);
+						*/
 					});
 					dialog.Show();
 				};
@@ -272,7 +333,6 @@ namespace SwdEditor
 
 		protected override async void OnCreate(Bundle bundle) {
 			base.OnCreate(bundle);
-			//ProgressBar pb = FindViewById<ProgressBar>(Resource.Id.progressBar1);
 			ProgressBar pb = new ProgressBar(this);
 			pb.Indeterminate = true;
 			pb.Visibility = ViewStates.Visible;
@@ -282,7 +342,6 @@ namespace SwdEditor
 
 			LinearLayout linearLayout = new LinearLayout(this);
 			linearLayout.Orientation = Orientation.Vertical;
-			//linearLayout.LayoutParameters = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.FillParent, ViewGroup.LayoutParams.FillParent);
 			SetContentView(linearLayout);
 
 			linearLayout.AddView(CreateTitleTable());
@@ -293,6 +352,14 @@ namespace SwdEditor
 			ScrollView scv = new ScrollView(this);
 			scv.AddView(mainTable);
 			linearLayout.AddView(scv);
+
+			TextView tv = new TextView(this);
+			tv.Text = mainTable.ChildCount.ToString();
+			tv.Gravity = GravityFlags.Center;
+			linearLayout.AddView(tv);
+
+			var toast = Toast.MakeText(this, "길게를 터치하면 편집", ToastLength.Long);
+			toast.Show();
 		}
 	}
 }
